55bc8593e34fc0d4453cfbae916b907e376175fb
Vaishali-1234 <150382344+Vaishali-1234@users.noreply.github.com>
2026-06-23 11:45:48 +0530

    Comment out main function and add isSorted method

--- 02_DSA/01_ARRAYS/02.cpp
+++ 02_DSA/01_ARRAYS/02.cpp
@@ -1,20 +1,35 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-int main(){
-    vector<int>vect1(3);
-    vect1.push_back(10);
-    vect1.push_back(20);
-    vect1.push_back(30);
-    for(int i : vect1){
-        cout<<i<<" ";
+// int main(){
+//     vector<int>vect1(3);
+//     vect1.push_back(10);
+//     vect1.push_back(20);
+//     vect1.push_back(30);
+//     for(int i : vect1){
+//         cout<<i<<" ";
+//     }
+
+
+
+//     // vect1.pop_back();
+//     // vect1.size();
+//     // vect1.clear();
+
+// }
+
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        // code here
+        for(int i =0 ; i<arr.size()-1; i++){
+            if(arr[i]<=arr[i+1]){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
-
-
-
-    // vect1.pop_back();
-    // vect1.size();
-    // vect1.clear();
-
-}
+};
