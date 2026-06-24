f0fda0ce26fba152c5be632dff7c2227fcc617f5
Vaishali-1234 <150382344+Vaishali-1234@users.noreply.github.com>
2026-06-24 06:10:20 +0000

    Added time complexity notes and examples

---
+++ 02_DSA/01_ARRAYS/09.cpp
@@ -0,0 +1,30 @@
+/*
+Time Complexity -> How much time will the algorithm takes as input size increases.
+
+Big O -> reprsents the upper bound of running time, it gives the worst case scenario of an alogorithm , 
+it describes the maximum time an algorithm can take
+
+1. O(1) -> Constant Time -> the time taken doesnt depend on imput size, the algorithm always runs in same time
+eg:
+cout<<"Hello";
+for(int i=0; i<5; i++){
+}
+
+2. O(logn) -> Logarathmic time - this grows logarathmically as input size increases, often seen in algorithm
+where search space is splitted in to half, like binary search
+
+
+3. O(n) -> linear time complexitu - time grows directly propotional to imput size
+for(int i=0; i<n; i++){
+    for(int j=0; j<5; j++){
+    }
+}
+
+4. O(nlogn) -> Linearthmic time - a mix of logarathmic and linear
+
+5. O(n^2) - quadratic time - time grows proportionally to square of input size 
+
+
+
+
+*/
